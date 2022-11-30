32a1ec699b85c53f026ade6cd044483ab9d68a36
LiBingbin <1525583255@qq.com>
2022-11-30 23:19:10 +0800

    commit

--- sequence/update_PC.py
+++ sequence/update_PC.py
@@ -2,4 +2,11 @@ from abstraction import *
 
 
 def select_PC_val(cpu):
-    return
+    if cpu.icode == 7:  # jXX
+        return cpu.valP
+    elif cpu.icode == 8:  # call
+        return cpu.valC
+    elif cpu.icode == 9:  # ret
+        return cpu.valM
+    else:
+        return cpu.valP
